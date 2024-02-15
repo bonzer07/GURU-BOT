2223befc76dca6bfd9a1f4623c4350db2b9840f5
bonzer07 <160163837+bonzer07@users.noreply.github.com>
2024-02-16 05:25:17 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['0134306095', 'Bonzer', true],
+  ['0134306095', 'Zam', true], 
   [''] 
 ] //Number of owners
 
